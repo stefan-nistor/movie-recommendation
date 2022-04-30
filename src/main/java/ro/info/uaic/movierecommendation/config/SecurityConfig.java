e384f0003948f9018442cfbca7c3232be1a2a81b
stefan.nistor <stefan.nistor@fortech.ro>
2022-04-30 14:53:57 +0300

    added remote database source

--- src/main/java/ro/info/uaic/movierecommendation/config/SecurityConfig.java
+++ src/main/java/ro/info/uaic/movierecommendation/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
 @Configuration
 @EnableWebSecurity
@@ -13,7 +14,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests().antMatchers("/health").permitAll();
         http.authorizeRequests().antMatchers("/swagger-ui/index.html").permitAll();
-
+        http.authorizeRequests().antMatchers("/login").permitAll();
     }
 
 }
