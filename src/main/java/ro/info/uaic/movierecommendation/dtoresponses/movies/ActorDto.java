0d46b7e4b0edd1018bdabcd3ce9e98e1f8dd4aa4
Irina Burada <irina.burada01@gmail.com>
2022-05-21 01:24:50 +0300

    add @NoArgsConstr

--- src/main/java/ro/info/uaic/movierecommendation/dtoresponses/movies/ActorDto.java
+++ src/main/java/ro/info/uaic/movierecommendation/dtoresponses/movies/ActorDto.java
@@ -2,15 +2,16 @@ package ro.info.uaic.movierecommendation.dtoresponses.movies;
 
 import com.sun.istack.NotNull;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 public class ActorDto {
 
     @NotNull
     private String name;
     private int numberOfAwards;
 
-    public ActorDto(){}
     public ActorDto(String name) {
         this.name = name;
     }
