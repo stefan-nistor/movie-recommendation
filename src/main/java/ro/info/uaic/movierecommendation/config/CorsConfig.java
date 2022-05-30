ccc56dbfe40389057cbb99577bf037879645d65a
Paul513607 <paul.samson42@gmail.com>
2022-05-30 21:17:56 +0300

    cors

--- src/main/java/ro/info/uaic/movierecommendation/config/CorsConfig.java
+++ src/main/java/ro/info/uaic/movierecommendation/config/CorsConfig.java
@@ -2,9 +2,11 @@ package ro.info.uaic.movierecommendation.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebMvc
 public class CorsConfig implements WebMvcConfigurer {
 
     @Override
