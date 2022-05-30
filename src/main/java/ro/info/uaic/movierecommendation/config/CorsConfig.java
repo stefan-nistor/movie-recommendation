b7ffb3e5c7a3d66f89670855fe1eab09242302a8
Paul513607 <paul.samson42@gmail.com>
2022-05-30 21:45:35 +0300

    Update CorsConfig.java

--- src/main/java/ro/info/uaic/movierecommendation/config/CorsConfig.java
+++ src/main/java/ro/info/uaic/movierecommendation/config/CorsConfig.java
@@ -2,7 +2,6 @@ package ro.info.uaic.movierecommendation.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -11,8 +10,6 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowedOrigins("*");
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS");
     }
 }
